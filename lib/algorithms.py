FIX: correct units are now used for the bandwidth, throughput, etc

lib/algorithms.py
@@ -1,9 +1,10 @@
 import numpy as np
 from scipy.optimize import root_scalar
-from lib.topology import Topology, AntennaModel, snr, pathloss_oh, pathloss_fs, dist2, Wcost, Wcost_prime
+from lib.topology import Topology, pathloss_oh, Wcost, Wcost_prime
 from lib.graph import WeightedGraph, WeightedEdge
 from lib.writer import write_log
 from lib.visualize import plot_allocated_bandwidth, plot_topology_allocation
+from typing import Callable
 
 
 def qos_density_graph(topo:Topology, p:tuple[float,float]) -> float:
@@ -27,17 +28,11 @@ def qos_density_graph(topo:Topology, p:tuple[float,float]) -> float:
     return sum / len(topo.graph.edges[p])
 
 
-def Wcost_simple(topo:Topology, u:tuple[float,float], p:tuple[float,float]) -> float:
-    """TODO
-    """
-    return topo.users[u].demand / np.log2(1 + snr(topo, p, u))
-
-
-def Wcost_dynamic(topo:Topology, u:tuple[float,float], p:tuple[float,float]) -> float:
+def compute_W_allocation(topo:Topology, u:tuple[float,float], p:tuple[float,float], pathloss:Callable[[Topology, tuple[float,float], tuple[float,float]], float]) -> float:
     """TODO
     """
     Q = topo.users[u].demand
-    PL = pathloss_fs(topo, p, u)
+    PL = pathloss(topo, p, u)
     N0 = -174
     antenna = topo.antennas[topo.pylons[p].antenna_type]
 
@@ -83,7 +78,7 @@ def get_closest_unallocated_ue(g:WeightedGraph, p:tuple[float,float]) -> Weighte
     return e
 
 
-def greedy_eu_bandwidth_allocation(topo:Topology, p:tuple[float,float], model:int, dynamic_cost:bool=True) -> float:
+def greedy_eu_bandwidth_allocation(topo:Topology, p:tuple[float,float], model:int, pathloss:Callable[[Topology, tuple[float,float], tuple[float,float]], float]) -> float:
     """Given an antenna, allocate bandwidth greedily starting with the closest EU.
 
     Parameters
@@ -98,8 +93,6 @@ def greedy_eu_bandwidth_allocation(topo:Topology, p:tuple[float,float], model:in
     Returns:
     The unallocated bandwidth of the Base Station
     """
-    Wcost = Wcost_dynamic if dynamic_cost else Wcost_simple
-
     # Set the antenna type and max bandwidth to allocate for the handled pylon
     topo.pylons[p].antenna_type = model
     Wmax = topo.antennas[model].bandwidth
@@ -107,7 +100,7 @@ def greedy_eu_bandwidth_allocation(topo:Topology, p:tuple[float,float], model:in
     e = get_closest_unallocated_ue(topo.graph, p)
     if e == None:
         return Wmax
-    Wc = Wcost(topo, e.v, p)
+    Wc = compute_W_allocation(topo, e.v, p, pathloss)
 
     plot_allocated_bandwidth(topo, p, e.v)
 
@@ -122,14 +115,14 @@ def greedy_eu_bandwidth_allocation(topo:Topology, p:tuple[float,float], model:in
         e = get_closest_unallocated_ue(topo.graph, p)
         if e == None:
             return Wmax
-        Wc = Wcost(topo, e.v, p)
+        Wc = compute_W_allocation(topo, e.v, p, pathloss)
     # Remove the remaining edges since the BS is already saturated
     topo.graph.edges[p] = []
     write_log(f"Can't allocate {Wc} MHz of bandwidth to {e.v}")# DEBUG
     return Wmax
 
 
-def greedy_allocation(topo:Topology) -> dict[tuple[float,float],str]:
+def greedy_allocation(topo:Topology, pathloss:Callable[[Topology, tuple[float,float], tuple[float,float]], float]) -> dict[tuple[float,float],str]:
     """Greedy algorithm to allocate pylons to end users.
 
     Parameters:
@@ -155,7 +148,7 @@ def greedy_allocation(topo:Topology) -> dict[tuple[float,float],str]:
     #return {sorted_pylons[0][0]: (topo.antennas[antenna_model].name, left_bandwidth, topo.antennas[antenna_model].bandwidth)}
 
     for p in sorted_pylons:
-        greedy_eu_bandwidth_allocation(topo, p[0], antenna_model, True)
+        greedy_eu_bandwidth_allocation(topo, p[0], antenna_model, pathloss)
         plot_topology_allocation(topo)
 
     # TODO: redo and rethink it

lib/topology.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json
+from typing import Callable
 from lib.graph import WeightedGraph
 from lib.writer import write_log
 
@@ -106,7 +107,7 @@ class Topology:
     tile_size:tuple[float,float]
     """(X,Y) size in meters of a density grid tile."""
     user_demand:float
-    """Demand of a user equipment in MHz."""
+    """Demand of a user equipment in Hz."""
     density_grid:np.array
     """2-dimensional density grid of end users."""
     graph:WeightedGraph
@@ -209,7 +210,7 @@ def pathloss_fs(topo:Topology, p:tuple[float,float], u:tuple[float,float]) -> fl
     # f = 3.5e9 # 3.5 GHz - ARCEP 5G urban model IN Hz
     f = 1.5e9 # 1.5 GHz - Okumura-Hata model upper bound IN Hz
     PL0 = 10*np.log10((4*np.pi*f)/(3*10**8))
-    d = dist2(p,u) # Convert distance between BS and UE
+    d = dist2(p,u) # Convert distance between BS and UE in meters
     return alpha * (PL0 + 10 * np.log10(d))
 
 
@@ -225,25 +226,33 @@ def pathloss_simple(topo:Topology, p:tuple[float,float], u:tuple[float,float]) -
     eta = 3. # Loss factor
     f = 1500 # 1.5 GHz - Okumura-Hata model upper bound IN MHz
     H = topo.pylons[p].height # Effective antenna height of the BS in meters (given by ARCEP2021)
-    d = .1 # 100m in km0
+    d = 0.1 # 100m in km
     PL0 = 69.55 + 26.16*np.log10(f) - 13.82*np.log10(H) + (44.9 - 6.55*np.log10(H)) * np.log10(d)
     return PL0 + eta*np.log10(dist2(p,u))
 
 
-def snr(topo:Topology, p:tuple[int,int], u:tuple[int,int]) -> float:
+def snr(topo:Topology, p:tuple[int,int], u:tuple[int,int], pathloss:Callable[[Topology, tuple[float,float], tuple[float,float]], float]) -> float:
     """Signal to Noise Ratio (SNR) not taking interferences between BS into account.
 
-    Parameters:
-    TODO
+    Parameters
+    ----------
+    topo
+        Topology object.
+    p
+        Position of the BS.
+    u
+        Position of the UE.
+    pathloss
+        Path loss model to use.
 
-    Returns:
-    float -- Linear SNR value.
+    Returns
+    -------
+    Linear SNR value.
     """
     antenna = topo.antennas[topo.pylons[p].antenna_type]
     N0 = -174 # dBm/Hz
-    SdB = antenna.power + antenna.gain - pathloss_fs(topo, p,u)
-    NdB = N0 * antenna.bandwidth
-    #return S / N
+    SdB = antenna.power + antenna.gain - pathloss(topo, p,u)
+    NdB = N0 + 10*np.log10(antenna.bandwidth)
     return np.float_power(10, (SdB - NdB)/10)
 
 
@@ -265,7 +274,7 @@ def Wcost(x:float, Q:float, N0:float, S:float) -> float:
     -------
     Cost value.
     """
-    print(x)
+    #print(x)
     return 10*np.log10(np.power(2, Q/x) - 1) + N0*x - S
 
 

lib/visualize.py
@@ -92,8 +92,6 @@ def plot_topology_allocation(topo:Topology):
     ax.plot(x, y, c='black', marker=r'$\bullet$', markersize=3, linestyle='none', label='User Equipments')
 
     max_bandwidth = np.max(list(map(lambda a: a.bandwidth, topo.antennas)))
-    print("Colormap test")#! DEBUG
-    print(f"Max bandwidth: {max_bandwidth}")#! DEBUG
     cbar = fig.colorbar(None, ax=ax, location='right', label='Allocated bandwidth (MHz)', cmap=test_cmap, norm=Normalize(0, max_bandwidth))
     cbar.minorticks_on()
 
@@ -115,7 +113,7 @@ def plot_allocated_bandwidth(topo:Topology, p:tuple[float,float], u:tuple[float,
 
     # Constants
     Q = topo.users[u].demand
-    PL = pathloss_fs(topo, p, u)
+    PL = pathloss_oh(topo, p, u)
     N0 = -174
     antenna = topo.antennas[topo.pylons[p].antenna_type]
     d = dist2(p,u)
@@ -161,63 +159,76 @@ def plot_allocated_bandwidth(topo:Topology, p:tuple[float,float], u:tuple[float,
 def plot_throughput(topo:Topology):
     # Compute values
     x = []
-    y = []
-    pl = []
-    s = []
-    sn = []
+    pathlosses = {"OH": pathloss_oh, "FS": pathloss_fs, "Simple": pathloss_simple}
+    y = {"OH": [], "FS": [], "Simple": []}
+    pl = {"OH": [], "FS": [], "Simple": []}
+    s = {"OH": [], "FS": [], "Simple": []}
+    sn = {"OH": [], "FS": [], "Simple": []}
 
+    # Measure everything
+    p = list(topo.pylons.keys())[0]# Select the only pylon in the topology
     for u in sorted(topo.users.keys(), key=lambda u: u[0]):
-        for p in topo.pylons.keys():
-            x.append(dist2(u, p))
-            # PL in db
-            y.append(topo.antennas[0].bandwidth * np.log2(1 + snr(topo, p, u)))
-            pl.append(pathloss_simple(topo, p,u))
-            s.append(topo.antennas[0].power + topo.antennas[0].gain - pathloss_simple(topo, p,u))
-            sn.append(snr(topo, p,u))
-            break
+        x.append(dist2(u, p))
+        for pi, pathloss in pathlosses.items():
+            y[pi].append(topo.antennas[0].bandwidth * np.log2(1 + snr(topo, p, u, pathloss)))
+            pl[pi].append(pathloss(topo, p,u))
+            s[pi].append(topo.antennas[0].power + topo.antennas[0].gain - pathloss(topo, p,u))
+            sn[pi].append(10*np.log10(snr(topo, p,u, pathloss)))
 
     # PL
     fig = plt.figure()
     ax = fig.add_subplot()
-    ax.scatter(x, pl, c='red')
+
+    for pi in pathlosses.keys():
+        ax.scatter(x, pl[pi], label=pi)
 
     ax.set_xlabel('Distance between the UE and BS (m)')
     ax.set_ylabel('Path loss (dB)')
-    ax.set_yscale('log')
+    #ax.set_yscale('log')
     ax.grid(True)
+    ax.legend()
 
     plt.show(block=False)
 
     # S
     fig = plt.figure()
     ax = fig.add_subplot()
-    ax.scatter(x, s, c='pink')
+
+    for pi in pathlosses.keys():
+        ax.scatter(x, s[pi], label=pi)
 
     ax.set_xlabel('Distance between the UE and BS (m)')
-    ax.set_ylabel('Signal in dB')
+    ax.set_ylabel('Signal (dB)')
     ax.grid(True)
+    ax.legend()
 
     plt.show(block=False)
 
     # SNR
     fig = plt.figure()
     ax = fig.add_subplot()
-    ax.scatter(x, sn, c='blue')
+
+    for pi in pathlosses.keys():
+        ax.scatter(x, sn[pi], label=pi)
 
     ax.set_xlabel('Distance between the UE and BS (m)')
-    ax.set_ylabel('Signal to Noise Ratio')
-    ax.set_yscale('log')
+    ax.set_ylabel('Signal to Noise Ratio (dB)')
+    #ax.set_yscale('log')
     ax.grid(True)
+    ax.legend()
 
     plt.show(block=False)
 
     # Throughput
     fig = plt.figure()
     ax = fig.add_subplot()
-    ax.scatter(x, y, c='black')
+
+    for pi in pathlosses.keys():
+        ax.scatter(x, y[pi], label=pi)
 
     ax.set_xlabel('Distance between the UE and BS (m)')
-    ax.set_ylabel('Throughput (Mbps)')
+    ax.set_ylabel('Throughput (bps)')
     ax.grid(True)
+    ax.legend()
 
     plt.show()